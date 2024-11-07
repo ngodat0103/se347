05926b695c808c8a99097060172c519c1a13faab
VietDucc <21521959@gm.uit.edu.vn>
2024-11-07 19:03:41 +0700

    VietDuc(login): Login form

---
+++ frontend/jira-clone/src/app/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button"
+import { LoginComponent } from "@/app/features/login"
+
+export default function Home() {
+  return (
+    <div>
+      <LoginComponent />
+    </div>
+    
+  )
+}
