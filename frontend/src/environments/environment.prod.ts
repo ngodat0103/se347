2b4083b30411ab2bc52457ca445266e7ec93b972
VietDucc <21521959@gm.uit.edu.vn>
2024-11-07 17:02:00 +0700

    VietDuc(frondend): Delete full FrontEnd

--- frontend/src/environments/environment.prod.ts
+++
@@ -1,7 +0,0 @@
-import { apiPath } from './../app/share/constance/api-path';
-export const environment = {
-  production: true,
-  apiUrl: 'http://'+window.location.hostname+':5000',
-  // apiUrl: 'https://api.hieud.me',
-  url: 'http://'+window.location.hostname+':4200'
-};
