ae401c36d8e20cea465807a892d70bf731cd159f
VietDucc <21521959@gm.uit.edu.vn>
2024-11-08 17:19:20 +0700

    VietDuc(signup-signin): create and done

--- frontend/jira-clone/src/app/page.tsx
+++ frontend/jira-clone/src/app/page.tsx
@@ -1,9 +1,18 @@
 import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
 import { LoginComponent } from "@/app/features/login"
 
 export default function Home() {
   return (
-    <div>
+    <div className= "">
+      <Button size="lg"> Primary 1</Button>
+      <Button variant="destructive">Destructive</Button>
+      <Button variant ="ghost">Ghost</Button>
+      <Button variant="secondary"> Primary</Button>
+      <Button variant="outline">Outline</Button>
+      <Button variant="muted">muted</Button>
+      <Button variant= "teritary">teritraty</Button>
+      <Input></Input>
       <LoginComponent />
     </div>
     
