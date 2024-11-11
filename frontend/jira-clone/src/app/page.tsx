4acfabda289cc72b8a7328292d743cbaded958ef
VietDucc <21521959@gm.uit.edu.vn>
2024-11-12 01:23:56 +0700

    VietDuc(landingpage): CardImage

--- frontend/jira-clone/src/app/page.tsx
+++ frontend/jira-clone/src/app/page.tsx
@@ -1,20 +1,11 @@
-import { Button } from "@/components/ui/button"
-import { Input } from "@/components/ui/input"
-import { LoginComponent } from "@/app/features/login"
-
+import SignUpPage from "./(auth)/sign-up/page";
+import SignInPage from "./(auth)/sign-in/page";
+import { Button } from "@/components/ui/button";
+import LandingPage from "./(landingpage)/page";
 export default function Home() {
   return (
-    <div className= "">
-      <Button size="lg"> Primary 1</Button>
-      <Button variant="destructive">Destructive</Button>
-      <Button variant ="ghost">Ghost</Button>
-      <Button variant="secondary"> Primary</Button>
-      <Button variant="outline">Outline</Button>
-      <Button variant="muted">muted</Button>
-      <Button variant= "teritary">teritraty</Button>
-      <Input></Input>
-      <LoginComponent />
+    <div className="">
+      <LandingPage />
     </div>
-    
-  )
+  );
 }
