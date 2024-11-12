f387bb217d10950824aa2c1645b68bd30bc78af5
Mai Phúc Minh <minhminh99599@gmail.com>
2024-11-12 20:21:28 +0700

    Clean up frontend files

--- frontend/jira-clone/src/app/(auth)/sign-up/page.tsx
+++
@@ -1,8 +0,0 @@
-"use client";
-import { Button } from "@/components/ui/button";
-import { SignUpCard } from "@/features/auth/components/sign-up-card";
-const SignUpPage = () => {
-  return <SignUpCard />;
-};
-
-export default SignUpPage;
