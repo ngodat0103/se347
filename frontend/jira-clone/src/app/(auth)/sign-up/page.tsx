806b25b1c445327e1c61af3103eb388fa996b18a
ngodat0103 <21521935@gm.uit.edu.vn>
2024-11-13 18:27:20 +0700

    resolve conflict

--- frontend/jira-clone/src/app/(auth)/sign-up/page.tsx
+++ frontend/jira-clone/src/app/(auth)/sign-up/page.tsx
@@ -2,7 +2,7 @@
 
 import { SignUpCard } from "@/features/auth/components/sign-up-card";
 const SignUpPage = () => {
-  return <SignUpCard />;
+    return <SignUpCard />;
 };
 
 export default SignUpPage;
