5ea70caa9c8a2639ee8e41dce3774625c9c097d5
Mai Phúc Minh <minhminh99599@gmail.com>
2024-12-17 20:16:48 +0700

    Add authentication guard to /workspaces route

--- src/middleware.ts
+++ src/middleware.ts
@@ -25,5 +25,5 @@ export async function middleware(request: NextRequest) {
 }
 
 export const config = {
-  matcher: ["/dashboard", "/setting"],
+  matcher: ["/dashboard", "/setting", "/workspaces", "/workspaces/:path*"],
 };
