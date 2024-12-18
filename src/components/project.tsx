3086ffce93ca48a0767ab5dc49e3515632a3edf9
ngodat0103 <ngovuminhdat@gmail.com>
2024-12-18 21:46:53 +0700

    refactor, add project component

---
+++ src/components/project.tsx
@@ -0,0 +1,46 @@
+"use client";
+
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { RiAddCircleFill } from "react-icons/ri";
+
+import { cn } from "@/lib/utils";
+import { useWorkspaceId } from "@/features/workspace/hook/use-workspace-id";
+import { useCreateProjectModal } from "@/features/project/hook/use-create-project-modal";
+
+export const Project = () => {
+  const pathname = usePathname();
+  const { open } = useCreateProjectModal();
+  const workspaceId = useWorkspaceId();
+  // const { data } = useGetProjects({ workspaceId });
+
+  return (
+    <div className="flex flex-col gap-y-2">
+      <div className="flex items-center justify-between">
+        <p className="text-xs uppercase text-neutral-500">Projects</p>
+        <RiAddCircleFill
+          className="size-5 text-neutral-500 cursor-pointer hover:opacity-75 transition"
+          onClick={open}
+        />
+      </div>
+      {/* {data?.documents.map((project) => {
+        const href = `/workspaces/${workspaceId}/projects/${project.$id}`;
+        const isActive = pathname === href;
+
+        return (
+          <Link key={project.$id} href={href}>
+            <div
+              className={cn(
+                "flex items-center gap-2.5 p-2.5 rounded-md hover:opacity-75 transition cursor-pointer text-neutral-500",
+                isActive && "bg-white shadow-sm hover:opacity-100 text-primary"
+              )}
+            >
+              <ProjectAvatar image={project.imageUrl} name={project.name} />
+              <span className="truncate">{project.name}</span>
+            </div>
+          </Link>
+        );
+      })} */}
+    </div>
+  );
+};
--- src/features/workspace/components/create-workspace-modal.tsx
+++ src/features/workspace/components/create-workspace-modal.tsx
@@ -3,7 +3,7 @@ import { ResponsiveModal } from "@/components/responsive-modal";
 
 import { CreateWorkspaceForm } from "./create-workspace-form";
 
-import { useCreateWorkspaceModal } from "@/hooks/use-create-workspace-modal";
+import { useCreateWorkspaceModal } from "@/features/workspace/hook/use-create-workspace-modal";
 
 export const CreateWorkspaceModal = () => {
   const { isOpen, setIsOpen, close } = useCreateWorkspaceModal();
