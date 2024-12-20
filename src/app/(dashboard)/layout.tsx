b306cd68a4fe9bca6f96d032e206515dc6d223f8
Mai Phúc Minh <minhminh99599@gmail.com>
2024-12-20 17:08:31 +0700

    Add Suspense tag to Dashboard layout

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -4,27 +4,31 @@ import { Sidebar } from "../../components/sidebar";
 import { Navbar } from "../../components/navbar";
 import { CreateWorkspaceModal } from "@/features/workspace/components/create-workspace-modal";
 import { CreateProjectModal } from "@/features/project/components/create-project-modal";
+import { Suspense } from 'react'
+
 interface DashboardLayoutProps {
   children: React.ReactNode;
 }
 
 const DashboardLayout = ({ children }: DashboardLayoutProps) => {
   return (
-    <div className="min-h-screen">
-      <CreateWorkspaceModal />
-      <CreateProjectModal/>
-      <div className="flex w-full h-full">
-        <div className="fixed left-0 top-0 hidden lg:block lg:w-[264px] h-full overflow-y-auto">
-          <Sidebar />
-        </div>
-        <div className="lg:pl-[264px] w-full">
-          <div className="mx-auto max-w-screen-2xl h-full">
-            <Navbar />
-            <main className="h-full py-8 px-6 flex flex-col">{children}</main>
+    <Suspense>
+      <div className="min-h-screen">
+        <CreateWorkspaceModal />
+        <CreateProjectModal />
+        <div className="flex w-full h-full">
+          <div className="fixed left-0 top-0 hidden lg:block lg:w-[264px] h-full overflow-y-auto">
+            <Sidebar />
+          </div>
+          <div className="lg:pl-[264px] w-full">
+            <div className="mx-auto max-w-screen-2xl h-full">
+              <Navbar />
+              <main className="h-full py-8 px-6 flex flex-col">{children}</main>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </Suspense>
   );
 };
 
