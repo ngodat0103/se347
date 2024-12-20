c5e0d53c6f0744f9e571cbecdb128f7e53855532
Mai Phúc Minh <minhminh99599@gmail.com>
2024-12-20 17:07:53 +0700

    Add NuqsAdapter to root layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
+import { NuqsAdapter } from 'nuqs/adapters/next/app'
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,7 +19,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={cn(inter.className, "antialiased min-h-screen")}>
-        <main>{children}</main>
+        <main>
+          <NuqsAdapter>{children}</NuqsAdapter>
+          </main>
       </body>
     </html>
   );
