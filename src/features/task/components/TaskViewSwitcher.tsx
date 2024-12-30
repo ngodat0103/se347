b0142e90aab63e1b35ba71bbde0c5ef230fde431
Mai Phúc Minh <minhminh99599@gmail.com>
2024-12-30 14:35:41 +0700

    Set isHideProjectFilter default to false

--- src/features/task/components/TaskViewSwitcher.tsx
+++ src/features/task/components/TaskViewSwitcher.tsx
@@ -17,13 +17,8 @@ import { DataCalendar } from "./data-calendar";
 import { useCallback } from "react";
 import { TaskStatus } from "@/types/task";
 import { updateMultipleTasks } from "@/services/taskService";
-interface TaskViewSwitcherProps {
-  isHideProjectFilter: boolean;
-}
 
-export const TaskViewSwitcher = ({
-  isHideProjectFilter,
-}: TaskViewSwitcherProps) => {
+export const TaskViewSwitcher = ({isHideProjectFilter = false}) => {
   const [view, setView] = useQueryState("task-view", { defaultValue: "table" });
   const { open } = useCreateTaskModal();
   const workspaceId = useWorkspaceId();
