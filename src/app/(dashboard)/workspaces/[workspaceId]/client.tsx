28841e2041ddc23ae5ad8c8a85a1c85811dbfaeb
Mai Phúc Minh <minhminh99599@gmail.com>
2025-01-01 20:53:53 +0700

    Add some padding

--- src/app/(dashboard)/workspaces/[workspaceId]/client.tsx
+++ src/app/(dashboard)/workspaces/[workspaceId]/client.tsx
@@ -56,7 +56,7 @@ export const WorkspaceIdClient = () => {
   return (
     <div className="h-full flex flex-col">
       <Analytics {...workspaceAna} />
-      <div className="grid grid-cols-1 xl:grid-cols-2 gap-4">
+      <div className="grid grid-cols-1 xl:grid-cols-2 gap-4 py-4">
         <div className="flex flex-col gap-4">
           <TaskList
             tasks={workspaceTasks}
