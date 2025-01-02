151ee5087bbd616be9116f16d55a7799697b8642
Ngoc Diem <21520186@gm.uit.edu.vn>
2025-01-03 00:55:05 +0700

    delete comment

--- src/features/task/components/tasks/task-description.tsx
+++ src/features/task/components/tasks/task-description.tsx
@@ -13,7 +13,7 @@ interface TaskDescriptionProps {
 }
 export const TaskDescription = ({ task,currentMember }: TaskDescriptionProps) => {
   const [isEditing, setIsEditing] = useState(false);
-  const [description, setDescription] = useState(task.description || ""); // Khởi tạo với chuỗi rỗng nếu task.description không tồn tại
+  const [description, setDescription] = useState(task.description || ""); 
 
   const { mutate, isPending } = updateTaskService();
 
