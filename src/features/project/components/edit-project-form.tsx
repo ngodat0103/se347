1b1a2abc9e8bd1ab7367971f1d0b1205e2a3b5ff
Nguyen Thi Yen Ly <nguyenlyyenthi@gmail.com>
2025-01-02 23:14:52 +0700

    delete comment 2

--- src/features/project/components/edit-project-form.tsx
+++ src/features/project/components/edit-project-form.tsx
@@ -50,7 +50,6 @@ export const EditProjectForm = ({
   const currentProjectId = useProjectId();
   const currentWorkspaceId = useWorkspaceId();
 
-
   const [DeleteDialog, confirmDelete,] = useConfirm(
     "Delete Project",
     "This action cannot be undone.",
